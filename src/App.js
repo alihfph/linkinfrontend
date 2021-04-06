2ee466ae1a2e192a50dd19e3babaad332ad81466
Can Soylu <hsyncn17@gmail.com>
2021-04-06 15:08:32 +0300

    https://github.com/Kravid-Z/linkendin-react-team.git

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import ProfilePage from "./Pages/ProfilePage";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavBar from "./Components/NavBar";
+import FooTer from "./Components/FooTer";
+
 function App() {
   return (
     <Router>
       <NavBar />
       <ProfilePage />
+      <FooTer />
     </Router>
   );
 }
