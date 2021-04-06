f779ae7be0591705de7076d379240f780d76d5d7
alihfph <ali.hfph@gmail.com>
2021-04-06 15:03:13 +0200

    navbar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import {FaLinkedin, FaNetworkWired} from 'react-icons/fa';
+import {FaLinkedin} from 'react-icons/fa';
+import { ImHome, ImDice, ImLibrary, ImPhone, ImClock2, ImDatabase, ImRocket } from "react-icons/im";
 import { Nav, Navbar, Form, FormControl} from 'react-bootstrap';
 
 export default class NavBar extends Component {
@@ -16,13 +17,13 @@ export default class NavBar extends Component {
           <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto my-auto text-center">
           </Nav>
-           <Nav className="text-center"><p><FaLinkedin/></p> <p> Home </p></Nav>
-           <Nav href="#Network"><FaNetworkWired/> <br/>My Network</Nav>
-           <Nav href="#Jobs"><FaNetworkWired/><br/> Jobs</Nav>
-           <Nav href="#Messaging"><FaLinkedin/> <br/> Messaging</Nav>
-           <Nav href="#Notification"><FaLinkedin/><br/> Notification</Nav>
-           <Nav href="#Me"><FaLinkedin/><br/> Me</Nav>
-           <Nav href="#Work"><FaLinkedin/><br/> Work</Nav>  
+           <Nav className="text-center mx-2" style={{display:"block"}}><div><ImHome/></div> <div> Home </div></Nav>
+           <Nav className="text-center mx-2" style={{display:"block"}}><div><ImDice/></div> <div> Home </div></Nav>
+           <Nav className="text-center mx-2" style={{display:"block"}}><div><ImLibrary/></div> <div> Home </div></Nav>
+           <Nav className="text-center mx-2" style={{display:"block"}}><div><ImPhone/></div> <div> Home </div></Nav>
+           <Nav className="text-center mx-2" style={{display:"block"}}><div><ImClock2/></div> <div> Home </div></Nav>
+           <Nav className="text-center mx-2" style={{display:"block"}}><div><ImDatabase/></div> <div> Home </div></Nav>
+           <Nav className="text-center mx-2" style={{display:"block"}}><div><ImRocket/></div> <div> Home </div></Nav>
            </Navbar.Collapse>
           </Navbar>
       </div>
