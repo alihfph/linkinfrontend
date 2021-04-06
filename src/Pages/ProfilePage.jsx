1e1836276bb927ed5fab4fe998ed8d8102cea1f6
ABskr <bskr26@gmx.de>
2021-04-06 12:50:05 +0200

    Layout done

--- src/Pages/ProfilePage.jsx
+++ src/Pages/ProfilePage.jsx
@@ -1,12 +1,24 @@
 import React, { Component } from "react";
+import { Col, Container, Row } from "react-bootstrap";
 import JumbotronProfilePage from "../Components/JumbotronProfilePage";
 
 export default class ProfilePage extends Component {
   render() {
     return (
-      <div>
-        <JumbotronProfilePage />
-      </div>
+      <Container>
+        <Row>
+          <Col xs={8}>
+            <JumbotronProfilePage />
+            {/* <Highlights />
+            <Bio /> */}
+          </Col>
+          <Col xs={4}>
+            {/* <AdvertisementBoxes />
+            <MutualFriends />
+            <Learning /> */}
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
