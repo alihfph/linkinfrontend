9e824e17ce3663940ab7b865762638f9b4bc6d87
ABskr <bskr26@gmx.de>
2021-04-06 13:27:15 +0200

    fetch 1 added

--- src/Pages/ProfilePage.jsx
+++ src/Pages/ProfilePage.jsx
@@ -3,19 +3,50 @@ import { Col, Container, Row } from "react-bootstrap";
 import JumbotronProfilePage from "../Components/JumbotronProfilePage";
 
 export default class ProfilePage extends Component {
+  state = {
+    userData: {}
+  }
+
+  getMyData = async () => {
+    const andisToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDZjMGM5YzZmZDIyODAwMTUzZmRiYWMiLCJpYXQiOjE2MTc2OTM4NTIsImV4cCI6MTYxODkwMzQ1Mn0.b_4i8l9HxOmAylxIxWyK1cX9Brjnydu_my16UsNd4PE'
+    try {
+      let resp = await fetch(
+        'https://striveschool-api.herokuapp.com/api/profile/me', {
+          headers: {
+            'Authorization' : 'Bearer ' + andisToken
+          }
+        }
+      );
+      let loggedInUser = await resp.json()
+      if (resp.ok) {
+        this.setState({
+          userData: loggedInUser
+        })
+      }
+
+    } catch (err) {
+      console.log(err)
+      alert(`There's an error. Check your console.`)
+    }
+  }
+
+  componentDidMount = () => {
+    this.getMyData()
+  }
   render() {
+    console.log(this.state)
     return (
       <Container>
         <Row>
           <Col xs={8}>
-            <JumbotronProfilePage />
+            <JumbotronProfilePage thisUser={this.state.userData}/>
             {/* <Highlights />
             <Bio /> */}
           </Col>
           <Col xs={4}>
             {/* <AdvertisementBoxes />
             <MutualFriends />
-            <Learning /> */}
+            <Learning />*/}
           </Col>
         </Row>
       </Container>
