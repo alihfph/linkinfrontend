d95a89dd37547fcebf15e5f14a85be4e443b0b5a
ABskr <bskr26@gmx.de>
2021-04-06 16:01:56 +0200

    NavBar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,5 +1,77 @@
-import React from "react";
+import React, { Component } from 'react';
+import { FaLinkedin } from 'react-icons/fa';
+import {
+  ImHome,
+  ImDice,
+  ImLibrary,
+  ImPhone,
+  ImClock2,
+  ImDatabase,
+  ImRocket,
+} from 'react-icons/im';
+import { Nav, Navbar, Form, FormControl, Container } from 'react-bootstrap';
 
-export default function NavBar() {
-  return <div></div>;
+export default class NavBar extends Component {
+  render() {
+    return (
+      <Container fluid>
+        <Navbar expand='lg'>
+          <Form inline>
+            <Navbar.Brand href='#home'>
+              <FaLinkedin
+                style={{ color: '	rgb(65,105,225)', fontSize: '40px' }}
+              />
+            </Navbar.Brand>
+            <FormControl type='text' placeholder='Search' className='' />
+          </Form>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='mr-auto my-auto text-center'></Nav>
+            <Nav className='text-center mx-4' style={{ display: 'block' }}>
+              <div>
+                <ImHome />
+              </div>{' '}
+              <div> Home </div>
+            </Nav>
+            <Nav className='text-center mx-4' style={{ display: 'block' }}>
+              <div>
+                <ImDice />
+              </div>{' '}
+              <div> Home </div>
+            </Nav>
+            <Nav className='text-center mx-4' style={{ display: 'block' }}>
+              <div>
+                <ImLibrary />
+              </div>{' '}
+              <div> Home </div>
+            </Nav>
+            <Nav className='text-center mx-4' style={{ display: 'block' }}>
+              <div>
+                <ImPhone />
+              </div>{' '}
+              <div> Home </div>
+            </Nav>
+            <Nav className='text-center mx-4' style={{ display: 'block' }}>
+              <div>
+                <ImClock2 />
+              </div>{' '}
+              <div> Home </div>
+            </Nav>
+            <Nav className='text-center mx-4' style={{ display: 'block' }}>
+              <div>
+                <ImDatabase />
+              </div>{' '}
+              <div> Home </div>
+            </Nav>
+            <Nav className='text-center mx-4' style={{ display: 'block' }}>
+              <div>
+                <ImRocket />
+              </div>{' '}
+              <div> Home </div>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </Container>
+    );
+  }
 }
