33e75df022df567c4eea5811d109d72c9d59b5e9
“Kravid-Z” <kravidz64@gmail.com>
2021-04-06 11:03:11 +0200

    Structure LinkedIn Project

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from '../styles/hero_style.module.css'
+
+export default function Hero() {
+    return (
+        <div className={styles.hello}>
+            <h1>Hello</h1>
+        </div>
+    )
+}
---
+++ src/Pages/ProfilePage.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Hero from "../Components/Hero";
+
+export default class ProfilePage extends Component {
+  render() {
+    return (
+      <div>
+        <Hero />
+      </div>
+    );
+  }
+}
