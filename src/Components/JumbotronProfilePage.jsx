30fcdfcc41de1370b97a421a3bd1ba8f757fcca0
ABskr <bskr26@gmx.de>
2021-04-06 13:30:55 +0200

    #3 Jumbotron added

--- src/Components/JumbotronProfilePage.jsx
+++ src/Components/JumbotronProfilePage.jsx
@@ -1,10 +1,35 @@
 import React from "react";
+import {Button, Card} from 'react-bootstrap'
 import styles from "../styles/jumbotron_profile_page_style.module.css";
 
-export default function Hero() {
+export default function Hero({thisUser}) {
   return (
-    <div className={styles.hello}>
-      <h1>Hello</h1>
-    </div>
+    <Card style={{ width: '100%' }}>
+      <Card.Img variant='top' src='https://picsum.photos/800/300' />
+      <Card.Body>
+        <div className={styles.imgWrapper}>
+          <img
+            className={styles.borderRound}
+            src={thisUser.image}
+            alt='profile pic'
+            style={({ width: '180px' }, { height: '180px' })}
+          />
+        </div>
+        <Card.Title className='mt-2'>
+          {thisUser.name} {thisUser.surname}
+        </Card.Title>
+        <h6>{thisUser.title}</h6>
+        <Card.Text>{thisUser.area}</Card.Text>
+        <Button className='rounded-pill' variant='primary'>
+          Open for...
+        </Button>
+        <Button className='mx-2 rounded-pill' variant='outline-secondary'>
+          Edit Profile
+        </Button>
+        <Button className='rounded-pill' variant='outline-secondary'>
+          More...
+        </Button>
+      </Card.Body>
+    </Card>
   );
 }
