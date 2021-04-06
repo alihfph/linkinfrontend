03ec8ea67202e2a460d89c3eeb7048eedbcb2d16
“Kravid-Z” <kravidz64@gmail.com>
2021-04-06 15:37:32 +0200

    Mutual card and UserCard

--- src/Components/MutualFriends.jsx
+++ src/Components/MutualFriends.jsx
@@ -1,14 +1,51 @@
-import React, { Component } from 'react'
-import Col from 'react-bootstrap/Col'
-import UserCard from './UserCard'
+import React, { Component } from "react";
+import { Card, ListGroup } from "react-bootstrap";
+import UserCard from "./UserCard";
 
 export default class MutualFriends extends Component {
-    render() {
-        return (
-            <Col>
-                <h2>People you may know</h2>
-                <UserCard/>
-            </Col>
-        )
+  state = {
+    mutualFriends: [],
+  };
+
+  getMutualFriends = async () => {
+    const davidToken =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDZjNGIzNTZmZDIyODAwMTUzZmRiY2UiLCJpYXQiOjE2MTc3MDk4NzgsImV4cCI6MTYxODkxOTQ3OH0.Oj8Gh3QwA30cjIFBe8hsuCJpduzXOjcNHFbI6hV3IvM";
+    try {
+      let resp = await fetch(
+        "https://striveschool-api.herokuapp.com/api/profile/",
+        {
+          headers: {
+            Authorization: "Bearer " + davidToken,
+          },
+        }
+      );
+      let mutualFriendsResp = await resp.json();
+      if (resp.ok) {
+        this.setState({
+          mutualFriends: mutualFriendsResp,
+        });
+      }
+    } catch (err) {
+      console.log(err);
     }
+  };
+  componentDidMount = () => {
+    this.getMutualFriends();
+  };
+
+  render() {
+    console.log(this.state.mutualFriends);
+    return (
+      <Card className='p-2'>
+        <Card.Title>People you may know</Card.Title>
+        <ListGroup>
+          {this.state.mutualFriends.slice(0, 4).map((user) => (
+            <ListGroup.Item key={user._id}>
+              <UserCard user={user} />
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+      </Card>
+    );
+  }
 }
--- src/Pages/ProfilePage.jsx
+++ src/Pages/ProfilePage.jsx
@@ -1,52 +1,55 @@
 import React, { Component } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import JumbotronProfilePage from "../Components/JumbotronProfilePage";
-import Bio from '../Components/Bio'
+import Bio from "../Components/Bio";
+import MutualFriends from "../Components/MutualFriends";
 
 export default class ProfilePage extends Component {
   state = {
-    userData: {}
-  }
+    userData: {},
+  };
 
   getMyData = async () => {
-    const andisToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDZjMGM5YzZmZDIyODAwMTUzZmRiYWMiLCJpYXQiOjE2MTc2OTM4NTIsImV4cCI6MTYxODkwMzQ1Mn0.b_4i8l9HxOmAylxIxWyK1cX9Brjnydu_my16UsNd4PE'
+    const andisToken =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDZjMGM5YzZmZDIyODAwMTUzZmRiYWMiLCJpYXQiOjE2MTc2OTM4NTIsImV4cCI6MTYxODkwMzQ1Mn0.b_4i8l9HxOmAylxIxWyK1cX9Brjnydu_my16UsNd4PE";
     try {
       let resp = await fetch(
-        'https://striveschool-api.herokuapp.com/api/profile/me', {
+        "https://striveschool-api.herokuapp.com/api/profile/me",
+        {
           headers: {
-            'Authorization' : 'Bearer ' + andisToken
-          }
+            Authorization: "Bearer " + andisToken,
+          },
         }
       );
-      let loggedInUser = await resp.json()
+      let loggedInUser = await resp.json();
       if (resp.ok) {
         this.setState({
-          userData: loggedInUser
-        })
+          userData: loggedInUser,
+        });
       }
-
     } catch (err) {
-      console.log(err)
-      alert(`There's an error. Check your console.`)
+      console.log(err);
+      alert(`There's an error. Check your console.`);
     }
-  }
+  };
 
   componentDidMount = () => {
-    this.getMyData()
-  }
+    this.getMyData();
+  };
   render() {
-    console.log(this.state)
+    console.log(this.state);
     return (
       <Container>
         <Row>
           <Col xs={8}>
             <JumbotronProfilePage thisUser={this.state.userData} />
             {/* <Highlights /> */}
-            <Bio bioText={this.state.userData.bio}/>
+            <Bio bioText={this.state.userData.bio} />
           </Col>
           <Col xs={4}>
-            {/* <AdvertisementBoxes />
             <MutualFriends />
+            {/* <AdvertisementBoxes />
+            
             <Learning />*/}
           </Col>
         </Row>
