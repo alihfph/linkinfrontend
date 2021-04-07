b685ad49e335dac6c0900ad4d3ae6bbd3f3d939a
“Kravid-Z” <kravidz64@gmail.com>
2021-04-07 13:25:33 +0200

    Experinces Component, ExpItem Component, Fecth (GET, POST)

---
+++ src/Components/ExpItem.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+
+export default function ExpItem(props) {
+  console.log(props.exp);
+  return (
+    <div>
+      <Row>
+        <Col xs={3}>
+          <img
+            style={{ height: "50px", width: "50px" }}
+            className="rounded"
+            src={props.exp.image}
+            alt="Logo employer"
+          />
+        </Col>
+        <Col xs={9}>
+          <strong>{props.exp.company}</strong> <br />
+          {props.exp.role}
+        </Col>
+      </Row>
+    </div>
+  );
+}
--- src/Components/Experiences.jsx
+++ src/Components/Experiences.jsx
@@ -1,17 +1,50 @@
-import React, { Component } from 'react';
-import {Card, ListGroup,} from 'react-bootstrap'
-import ExpItem from './ExpItem'
+import React, { Component } from "react";
+import { Card, ListGroup } from "react-bootstrap";
+import ExpItem from "./ExpItem";
 
 class Experiences extends Component {
+  // state = {
+
+  //   myNewExp: {
+  //     role: "",
+  //     company: "",
+  //     startDate: "",
+  //     endDate: null, //could be null
+  //     description: "",
+  //     area: "",
+  //     image: "https://picsum.photos/200",
+  //   },
+  // };
+  // postNewExp = async () => {
+  //   try {
+  //     const andisToken =
+  //       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDZjMGM5YzZmZDIyODAwMTUzZmRiYWMiLCJpYXQiOjE2MTc2OTM4NTIsImV4cCI6MTYxODkwMzQ1Mn0.b_4i8l9HxOmAylxIxWyK1cX9Brjnydu_my16UsNd4PE";
+  //     let resp = await fetch(
+  //       `https://striveschool-api.herokuapp.com/api/profile/${this.props.userID}/experiences`,
+  //       {
+  //         method: "POST",
+  //         body: JSON.stringify(this.state.myNewExp),
+  //         headers: {
+  //           "Content-type": "application/json",
+  //           Authorization: "Bearer " + andisToken,
+  //         },
+  //       }
+  //     );
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+  // componentDidMount = () => {
+  //   this.postNewExp();
+  // };
   render() {
-    console.log(this.props.expData);
     return (
-      <Card className='p-2'>
+      <Card className="p-2">
         <Card.Title>Experiences</Card.Title>
         <ListGroup>
           {this.props.expData.map((exp) => (
-            <ListGroup.Item className='border-0' key={exp._id}>
-              <ExpItem exp={exp}/>
+            <ListGroup.Item className="border-0" key={exp._id}>
+              <ExpItem exp={exp} />
             </ListGroup.Item>
           ))}
         </ListGroup>
