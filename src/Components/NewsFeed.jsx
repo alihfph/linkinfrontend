9099585c55e4c2b1e1a55f6201090e9d07f48033
ABskr <bskr26@gmx.de>
2021-04-08 13:28:40 +0200

    misc

--- src/Components/NewsFeed.jsx
+++ src/Components/NewsFeed.jsx
@@ -7,16 +7,18 @@ class NewsFeed extends Component {
    
     return (
       <>
-        {this.props.posts.length>0 && this.props.posts.map((post) =>  post.user &&(
+        {this.props.posts.length>0 && this.props.posts.map((post) => post.user &&(
           <Card key={post._id}>
             {console.log(post.user)}
             <Card.Body>
               <div className='d-flex d-flex justify-content-between m-0 p0'>
                 <div className='m-0 p0'>
+                  <div className='m-0 p0'>
                   <Card.Title>{post.user.name}</Card.Title>
                   <Card.Subtitle className='mb-2 text-muted'>
                     {post.user.title ? post.user.title : 'no title'}
                   </Card.Subtitle>
+                  </div>
                 </div>
                 <div className='m-0 p0'>
                   <BsThreeDots />
