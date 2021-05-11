a7fb2eae955b59c553d95df6bf87cca35655b6f2
alihfph <ali.hfph@gmail.com>
2021-05-11 23:22:23 +0200

    basic sing and signup

--- src/App.js
+++ src/App.js
@@ -1,37 +1,42 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ProfilePage from "./Pages/ProfilePage";
-import OtherProfile from "./Pages/OtherProfile"
+import OtherProfile from "./Pages/OtherProfile";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from "./Components/NavBar";
 import FooTer from "./Components/FooTer";
 import Homepage from "./Pages/Homepage";
-import SignUp from "./Pages/SignUpPage"
+import SignUp from "./Pages/SignUpPage";
+import Signin from "./Pages/SigninPage";
 function App() {
   return (
     <Router>
       <NavBar />
       <Switch>
-      <Route
-        path = "/"
-        exact
-        render = {
-            (routeProps) => < Homepage {
-              ...routeProps
-            }
-            />} />
-      <Route
-        path="/profile/me"
-        exact
-        render={(routeProps) => <ProfilePage {...routeProps} />}
-      />
         <Route
-        path="/signup"
-        exact
-        render={(routeProps) => <SignUp
-         {...routeProps} />}
-      />
-      <Route path='/profile/:userId' render={(routeProps) => <OtherProfile {...routeProps} />} />
+          path="/"
+          exact
+          render={(routeProps) => <Homepage {...routeProps} />}
+        />
+        <Route
+          path="/profile/me"
+          exact
+          render={(routeProps) => <ProfilePage {...routeProps} />}
+        />
+        <Route
+          path="/signup"
+          exact
+          render={(routeProps) => <SignUp {...routeProps} />}
+        />
+        <Route
+          path="/signin"
+          exact
+          render={(routeProps) => <Signin {...routeProps} />}
+        />
+        <Route
+          path="/profile/:userId"
+          render={(routeProps) => <OtherProfile {...routeProps} />}
+        />
       </Switch>
       <FooTer />
     </Router>
